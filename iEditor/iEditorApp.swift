b248a256d1b14283b5ff5b3e32f8c4d7ee7c1bf4
Atikul Gazi <atiksplanet@gmail.com>
2022-10-07 01:41:22 +0600

    iEditor Project Created

---
+++ iEditor/iEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  iEditorApp.swift
+//  iEditor
+//
+//  Created by Atik on 7/10/22.
+//
+
+import SwiftUI
+
+@main
+struct iEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
